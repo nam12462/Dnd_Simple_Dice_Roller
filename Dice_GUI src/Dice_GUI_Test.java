523339e9c45580d83ddbf6dbe4f2f33e558182a7
Eric <nam12462@users.noreply.github.com>
2020-10-07 19:37:58 -0500

    Add files via upload

---
+++ Dice_GUI src/Dice_GUI_Test.java
@@ -0,0 +1,9 @@
+import javax.swing.JFrame;
+
+public class Dice_GUI_Test {
+	public static void main(String[] args) {
+		Dice_GUI test = new Dice_GUI();
+		test.setVisible(true);
+		test.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+}
